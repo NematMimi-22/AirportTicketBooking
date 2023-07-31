7ea722d4123e9ce4dcabb49c463d45e0c1802cf6
Nemat Mimi <nematmimi01@gmail.com>
2023-07-31 10:44:04 +0300

    Read from csv file

---
+++ FlightFileImporter.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBooking
+{
+    internal class FlightFileImporter
+    {
+        List<Flight> flights = new List<Flight>();
+        public List<Flight> ImportFlightsFromCsv(string filePath)
+        {
+
+
+            List<Flight> flights = new List<Flight>();
+
+
+            try
+            {
+                using (var reader = new StreamReader(filePath))
+                using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+                {
+                    flights = csv.GetRecords<Flight>().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            return flights;
+
+
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AirportTicketBooking;
+
+string filePath = "C:\\Users\\Nemat\\Desktop\\test.csv";
+
+FlightFileImporter flightImporter = new FlightFileImporter();
+
+List<Flight> flights = flightImporter.ImportFlightsFromCsv(filePath);
