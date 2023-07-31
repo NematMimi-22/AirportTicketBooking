3f804344f135c146da4ea761a89cffa7d06855a5
Nemat Mimi <nematmimi01@gmail.com>
2023-07-31 09:18:56 +0300

    Add project files.

---
+++ Booking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBooking
+{ 
+public enum FlightClass
+{
+    Economy,
+    Business,
+    FirstClass
+}
+
+
+    internal class Booking
+    {
+
+
+        public Flight Flight { get; set; }
+        public Passenger Passenger { get; set; }
+        public FlightClass FlightClass { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Flight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBooking
+{
+    internal class Flight
+    {
+        public string FlightNum { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public decimal EconomyClassPrice { get; set; }
+        public decimal BusinessClassPrice { get; set; }
+        public decimal FirstClassPrice { get; set; }
+    }
+}
